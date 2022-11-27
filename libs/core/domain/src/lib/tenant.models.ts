04cdb84d53e3acbef1c899e053f5fd4f20d5fdfd
Garrett Myrick <garrettmyrick@gmail.com>
2022-11-27 15:07:24 -0800

    added tenant models

---
+++ libs/core/domain/src/lib/tenant.models.ts
@@ -0,0 +1,45 @@
+import { abstract, BaseModel, Class, entity, input, output, Property, unique } from "@garrettmk/class-schema";
+import { CreateInput, ObjectFilterInput, Paginated, Pick, UpdateInput, WhereInput, WhereOneInput } from "@nest-mikro-tenants/core/factories";
+
+@Class({ output, entity, description: 'An application tenant'})
+export class Tenant extends BaseModel {
+    @Property(() => String, { unique, minLength: 2 })
+    name!: string;
+
+    @Property(() => String, { unique, minLength: 2 })
+    slug!: string;
+
+    @Property(() => Date, { default: () => new Date() })
+    createdAt!: Date;
+
+    @Property(() => Date, { default: () => new Date() })
+    updatedAt!: Date;
+}
+
+@Class({ input })
+export class TenantCreateInput extends CreateInput(Tenant, {
+    required: ['name', 'slug'],
+    omitted: ['createdAt', 'updatedAt'],
+    abstract
+}) {}
+
+@Class({ input })
+export class TenantUpdateInput extends UpdateInput(Tenant, {
+    omitted: ['createdAt', 'updatedAt'],
+    abstract
+}) {}
+
+@Class({ input })
+export class TenantFilterInput extends ObjectFilterInput(Tenant) {}
+
+@Class({ input })
+export class TenantFilterOneInput extends Pick(TenantFilterInput, ['id', 'name', 'slug']) {}
+
+@Class({ input })
+export class TenantsWhereInput extends WhereInput(Tenant, TenantFilterInput) {}
+
+@Class({ input })
+export class TenantsWhereOneInput extends WhereOneInput(Tenant, TenantFilterOneInput) {}
+
+@Class({ output })
+export class PaginatedTenants extends Paginated(Tenant) {}
