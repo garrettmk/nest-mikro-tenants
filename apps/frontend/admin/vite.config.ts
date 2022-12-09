dc03244873c87095c1225bde1ba8418781e0c808
Garrett Myrick <garrettmyrick@gmail.com>
2022-12-09 14:38:41 -0800

    admin: updated vite config

--- apps/frontend/admin/vite.config.ts
+++ apps/frontend/admin/vite.config.ts
@@ -3,10 +3,49 @@
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    build: {
+        commonjsOptions: {
+            // This fixed 'exports is not defined' errors caused by some
+            // third-party dependencies (class-transformer-validator, in this case)
+            transformMixedEsModules: true,
+        },
+    },
+    plugins: [
+        // Point tsConfigPaths to the root of the project
+        tsconfigPaths({
+            loose: true,
+            root: '../../../',
+        }),
+
+        // Although vite itself has no problem with decorators in TypeScript, they cause
+        // qwik to throw an error. But they are used extensively in the domain objects
+        // library, so we have to transpile them away before they reach qwik
+        babel({
+            filter: /\.(ts|js)?$/,
+            babelConfig: {
+                // Ignore the .babelrc files in library folders
+                babelrc: false,
+
+                plugins: [
+                    ['@babel/plugin-transform-typescript'],
+
+                    // legacy: true fixes errors about using 'export' between a decorator
+                    // and a class
+                    ['@babel/plugin-proposal-decorators', { legacy: true }],
+
+                    // Not sure if loose: true is necessary...
+                    ['@babel/plugin-proposal-class-properties', { loose: true }],
+                ],
+            },
+        }),
+
+        qwikCity(),
+        qwikVite(),
+    ],
     preview: {
         headers: {
             'Cache-Control': 'public, max-age=600',
