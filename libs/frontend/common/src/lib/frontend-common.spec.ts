5d53e119ea021c23d2f162c34b282d63db2f8bb2
Garrett Myrick <garrettmyrick@gmail.com>
2022-12-09 17:50:53 -0800

    fronted/common: added library to workspace

---
+++ libs/frontend/common/src/lib/frontend-common.spec.ts
@@ -0,0 +1,7 @@
+import { frontendCommon } from './frontend-common';
+
+describe('frontendCommon', () => {
+    it('should work', () => {
+        expect(frontendCommon()).toEqual('frontend-common');
+    });
+});
---
+++ libs/frontend/common/src/lib/frontend-common.ts
@@ -0,0 +1,3 @@
+export function frontendCommon(): string {
+    return 'frontend-common';
+}
